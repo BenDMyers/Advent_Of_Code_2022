27bc8e46e0f8c577e1a0769d7756e81394e77f0b
Ben Myers <ben.myers@okstate.edu>
2022-12-03 10:37:20 -0600

    Day 3 - Part 2

--- 03/index.ts
+++ 03/index.ts
@@ -3,12 +3,6 @@ import fs from 'fs';
 const rucksacks = fs
 	.readFileSync(`${__dirname}/.input`, 'utf-8')
 	.split('\n')
-	.map(rucksack => {
-		const midpoint = rucksack.length / 2;
-		const firstCompartment = rucksack.substring(0, midpoint);
-		const secondCompartment = rucksack.substring(midpoint);
-		return [firstCompartment, secondCompartment];
-	});
 
 // Part 1
 function intersection(firstCompartment: string[], secondCompartment: string[]) {
@@ -29,8 +23,15 @@ function getPriority(letter: string) {
 	return 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf(letter) + 1;
 }
 
+const compartmentalizedRucksacks = rucksacks.map(rucksack => {
+	const midpoint = rucksack.length / 2;
+	const firstCompartment = rucksack.substring(0, midpoint);
+	const secondCompartment = rucksack.substring(midpoint);
+	return [firstCompartment, secondCompartment];
+});
+
 let part1Total = 0;
-for (const [firstCompartment, secondCompartment] of rucksacks) {
+for (const [firstCompartment, secondCompartment] of compartmentalizedRucksacks) {
 	const rucksackErrors = intersection(
 		firstCompartment.split(''),
 		secondCompartment.split('')
@@ -43,3 +44,39 @@ for (const [firstCompartment, secondCompartment] of rucksacks) {
 }
 
 console.log(part1Total);
+
+// Part 2
+const elfTriads = rucksacks.reduce((triads, elf, index) => {
+	const currentTriad = triads[triads.length - 1];
+	currentTriad?.push(elf);
+
+	if (currentTriad?.length === 3 && index < rucksacks.length - 1) {
+		triads.push(new Array<string>());
+	}
+
+	return triads;
+}, [new Array<string>()]);
+
+function generalizedIntersection(...lists: string[][]) {
+	const [firstSet, ...remainingSets] = lists.map(list => new Set(list));
+	const intersection = new Set<string>();
+
+	for (let item of firstSet) {
+		if (remainingSets.every(set => set.has(item))) {
+			intersection.add(item);
+		}
+	}
+
+	return [...intersection];
+}
+
+let part2Total = 0;
+elfTriads.forEach(triad => {
+	const rucksackItems = triad.map(rucksack => rucksack.split(''));
+	const itemsInCommon = generalizedIntersection(...rucksackItems);
+	for (const item of itemsInCommon) {
+		part2Total += getPriority(item);
+	}
+});
+
+console.log(part2Total);
