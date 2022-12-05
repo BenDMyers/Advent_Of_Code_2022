b680c3a7814bbd64a7bd5e35919079ddbb648be1
Ben Myers <ben.myers@okstate.edu>
2022-12-04 23:44:49 -0600

    Day 5 - Part 2

--- 05/README.md
+++ 05/README.md
@@ -1,1 +1,1 @@
-# [Day 1](https://adventofcode.com/2022/day/1)
+# [Day 5](https://adventofcode.com/2022/day/5)
--- 05/index.ts
+++ 05/index.ts
@@ -12,7 +12,7 @@ const crateLines = lines.slice(0, crateNumbersIndex);
 const instructionLines = lines.slice(instructionsIndex);
 
 // Parse provided crate stacks
-const stacks: {[key: string]: string[]} = {};
+const initialStacks: {[key: string]: string[]} = {};
 const crateRowsBottomFirst = [...crateLines].reverse();
 for (const row of crateRowsBottomFirst) {
 	// Every crate is four characters (left bracket, letter, right bracket, space)
@@ -21,16 +21,16 @@ for (const row of crateRowsBottomFirst) {
 	cratesInRow.forEach((letters, stackIndex) => {
 		if (letters.length > 0) {
 			const [crateName] = letters;
-			if (!stacks[stackIndex + 1]) {
-				stacks[stackIndex + 1] = [] as string[];
+			if (!initialStacks[stackIndex + 1]) {
+				initialStacks[stackIndex + 1] = [] as string[];
 			}
-			stacks[stackIndex + 1].push(crateName);
+			initialStacks[stackIndex + 1].push(crateName);
 		}
 	});
 }
 
-// Parse provided instructions
-// type Instruction = {amount: number; from: number; to: number;}
+// Part 1
+const part1Stacks: {[key: string]: string[]} = JSON.parse(JSON.stringify(initialStacks));
 for (const instructionLine of instructionLines) {
 	const [, amountString, , fromString, , toString] = instructionLine.split(' ');
 	const amount = Number.parseInt(amountString);
@@ -38,15 +38,37 @@ for (const instructionLine of instructionLines) {
 	const to = Number.parseInt(toString);
 
 	for (let i = 0; i < amount; i++) {
-		const topCrate = stacks[from].pop();
-		topCrate && stacks[to].push(topCrate);
+		const topCrate = part1Stacks[from].pop();
+		topCrate && part1Stacks[to].push(topCrate);
 	}
 }
 
-let topCrates = '';
-for (const stack of Object.values(stacks)) {
+let part1TopCrates = '';
+for (const stack of Object.values(part1Stacks)) {
 	if (stack.length) {
-		topCrates += stack[stack.length - 1];
+		part1TopCrates += stack[stack.length - 1];
 	}
 }
-console.log(topCrates);
+console.log(part1TopCrates);
+
+// Part 2
+const part2Stacks: {[key: string]: string[]} = JSON.parse(JSON.stringify(initialStacks));
+for (const instructionLine of instructionLines) {
+	const [, amountString, , fromString, , toString] = instructionLine.split(' ');
+	const amount = Number.parseInt(amountString);
+	const from = Number.parseInt(fromString);
+	const to = Number.parseInt(toString);
+
+	const indexOfBottomLiftedCrate = part2Stacks[from].length - amount;
+	const liftedCrates = part2Stacks[from].slice(indexOfBottomLiftedCrate);
+	part2Stacks[from] = part2Stacks[from].slice(0, indexOfBottomLiftedCrate);
+	part2Stacks[to].push(...liftedCrates);
+}
+
+let part2TopCrates = '';
+for (const stack of Object.values(part2Stacks)) {
+	if (stack.length) {
+		part2TopCrates += stack[stack.length - 1];
+	}
+}
+console.log(part2TopCrates);
