63b795c6e40f50bbb7357d179ca29042e082e228
Ben Myers <ben.myers@okstate.edu>
2022-12-10 11:41:53 -0600

    Add a getLegLength util, just in case

--- utils/pythagorean-theorem.ts
+++ utils/pythagorean-theorem.ts
@@ -9,3 +9,16 @@ export function getHypotenuseLength(legA: number, legB: number) {
 	const b_2 = legB * legB;
 	return Math.sqrt(a_2 + b_2);
 }
+
+/**
+ * Gets the length of a right triangle's unknown leg, given the
+ * lengths of its hypotenuse and its known leg.
+ * @param hypotenuse length of the hypotenuse
+ * @param knownLeg length of the known leg
+ * @returns length of the other leg
+ */
+export function getLegLength(hypotenuse: number, knownLeg: number) {
+	const c_2 = hypotenuse * hypotenuse;
+	const a_2 = knownLeg * knownLeg;
+	return Math.sqrt(c_2 - a_2);
+}
